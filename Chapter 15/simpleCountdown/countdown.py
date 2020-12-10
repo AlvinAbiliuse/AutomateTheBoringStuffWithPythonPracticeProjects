bf86ffb0694992dffa4f99bcd5b948dd745aa4ab
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-11 02:37:08 +0400

    added sleep inside while loop with sleep time set to 1

--- Chapter 15/simpleCountdown/countdown.py
+++ Chapter 15/simpleCountdown/countdown.py
@@ -5,7 +5,10 @@ from sys import argv
 def countdown():
 	try:
 		print(f'Countdown Set for {argv[1]}')
-		time.sleep(int(argv[1]))
+		timeLeft = int(argv[1])
+		while timeLeft > 0:
+			time.sleep(1)
+			timeLeft -= 1
 		print('time\'s up')
 		subprocess.Popen(['see', 'alarm.wav'])
 	except IndexError:
