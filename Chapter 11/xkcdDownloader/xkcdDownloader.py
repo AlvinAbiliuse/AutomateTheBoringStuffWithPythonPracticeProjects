5dad0198c71d5ec890e0fbd3bbb8b6b23fa42b74
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-11 10:23:50 +0400

    added print functions

--- Chapter 11/xkcdDownloader/xkcdDownloader.py
+++ Chapter 11/xkcdDownloader/xkcdDownloader.py
@@ -5,6 +5,7 @@ def xkcdParse():
 	num = 1
 	while True:
 		siteData = requests.get('http://xkcd.com/' + str(num))
+		print('Getting site info')
 		if python.status_code == 200:
 			siteBS = bs4.BeautifulSoup(siteData)
 			elem = siteBS.select('#comic img')
@@ -12,15 +13,16 @@ def xkcdParse():
 			xkcdDownloader(imgLink, num)
 			num += 1
 		else:
+			print('The End!')
 			break
 
 
 def xkcdDownloader(link, number):
 	imageData = requests.get(link)
 	imageData.raise_for_status()
+	print(f'downloading {number}.png')
 	with open(f'./{number}.png', 'wb') as imageFile:
 		for i in imageData.iter_content(100000):
-			imageFile.write(i)
-	
+			imageFile.write(i)	
 
 xkcdParse()
