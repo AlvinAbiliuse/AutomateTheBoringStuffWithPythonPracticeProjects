afa3b39c2a5a97755204c3432d2ba48d60f549e4
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-25 04:07:47 +0400

    Something

---
+++ Chapter 15/ScheduledComicDownloader/xkcdDownloader.py
@@ -0,0 +1,28 @@
+import requests
+import bs4
+
+def xkcdParse():
+	num = 1
+	while True:
+		siteData = requests.get('https://xkcd.com/' + str(num))
+		print('Getting site info')
+		if siteData.status_code == 200:
+			siteBS = bs4.BeautifulSoup(siteData.text, features='lxml')
+			elem = siteBS.select('#comic img')
+			imgLink = elem[0].get('src')[2:]
+			xkcdDownloader(imgLink, num)
+			num += 1
+		else:
+			print('The End!')
+			break
+
+
+def xkcdDownloader(link, number):
+	imageData = requests.get('https://' + link)
+	imageData.raise_for_status()
+	print(f'downloading {number}.png')
+	with open(f'./{number}.png', 'wb') as imageFile:
+		for i in imageData.iter_content(100000):
+			imageFile.write(i)	
+
+xkcdParse()
