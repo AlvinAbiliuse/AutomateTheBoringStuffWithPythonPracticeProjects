2b3b3525d4a159878e0db08fe96df8bc3f9d8dce
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-27 05:46:22 +0400

    Thread Demo

--- Chapter 11/selPractice.py
+++ Chapter 11/selPractice.py
@@ -2,8 +2,13 @@ from selenium import webdriver
 
 browser = webdriver.Firefox(executable_path = './geckodriver')
 browser.get('http://inventwithpython.com')
-try:
-	elem = browser.find_element_by_class_name('bookcover')
-	print(f'Found {elem.tag_name} element with that class name!')
-except:
-	print('Was not able to find an element with that name.')
+linkElem = browser.find_element_by_link_text('Available in November 2020, but you can use discount code PREORDER for 25% off.')
+linkElem.click()
+
+
+
+#try:
+#	elem = browser.find_element_by_class_name('bookcover')
+#	print(f'Found {elem.tag_name} element with that class name!')
+#except:
+#	print('Was not able to find an element with that name.')
