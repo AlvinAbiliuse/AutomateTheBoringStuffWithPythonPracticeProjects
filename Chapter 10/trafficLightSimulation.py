7303a4dee2c3845675428619095004fe6e4b193b
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-28 05:01:00 +0400

    commit

--- Chapter 10/trafficLightSimulation.py
+++ Chapter 10/trafficLightSimulation.py
@@ -3,12 +3,16 @@ market_2nd = {'ns' : 'green', 'ew' : 'red'}
 mission_16th = {'ns': 'red', 'ew': 'green'}
 
 def switchLights(stopLight):
-	for key in stopLight.keys():
-		if stopLight[key] == 'green':
-			stopLight[key] = 'yellow'
-		if stopLight[key] == 'yellow':
-			stopLight[key] = 'red'
-		if stopLight[key] == 'red':
-			stopLight[key] = 'green'
+	for i in range(10):
+		for key in stopLight.keys():
+			if stopLight[key] == 'green':
+				stopLight[key] = 'yellow'
+			if stopLight[key] == 'yellow':
+				stopLight[key] = 'red'
+			if stopLight[key] == 'red':
+				stopLight[key] = 'green'
+		print(market_2nd)
+		print(mission_16th)
+		print('\n')
 
 switchLights(market_2nd)
