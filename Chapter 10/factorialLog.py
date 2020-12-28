59d791a57393998fa88487e34d4945d3e1d98ca0
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-29 03:31:56 +0400

    debugged factorialLog.py

--- Chapter 10/factorialLog.py
+++ Chapter 10/factorialLog.py
@@ -6,9 +6,9 @@ logging.basicConfig(level=logging.DEBUG, format=f'%(asctime)s - %(levelname)s - 
 def factorial(n):
 	logging.debug(f'Start of factorial ({n})')
 	total = 1
-	for i in range(n+1):
-		logging.debug(f'i is {i}, total is {total}')
+	for i in range(1, n+1):
 		total *= i
+		logging.debug(f'i is {i}, total is {total}')
 	return total
 
 print(factorial(5))
