2b461df4399cba993c3ffdc11bc776657f2a6632
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-29 09:40:57 +0400

    sameName.py

---
+++ Chapter 3/sameName.py
@@ -0,0 +1,13 @@
+def spam():
+	eggs = 'spam local'
+	print(eggs)	# prints 'spam local'
+
+def bacon():
+	eggs = 'bacon local'
+	print(eggs)	# prints 'bacon local'
+	span()
+	print(eggs)	# prints 'bacon local'
+
+egs = 'global'
+bacon()
+print(eggs)
