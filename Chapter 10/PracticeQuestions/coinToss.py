2ae59ecf9e5517f52243fca8431a16832170b165
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-29 04:11:27 +0400

    added logging to cointToss.py

--- Chapter 10/PracticeQuestions/coinToss.py
+++ Chapter 10/PracticeQuestions/coinToss.py
@@ -1,14 +1,25 @@
 import random
+import logging
+
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+logging.debug('Start of the program')
+
 guess = ''
 while guess not in ('heads', 'tails'):
 	print('Guess the coin toss! Enter Heads or tails')
 	guess = input().lower()
+
 toss = random.randint(0, 1)
+logging.debug(f'Guess is {guess}')
+logging.debug(f'toss is {toss}')
+
 if toss == guess:
 	print('You got it!')
 else:
 	print('Nope! Guess again!')
 	guess = input()
+	logging.debug(f'Second guess is {guess}')
+	logging.debug(f'toss is {toss}') 
 	if toss == guess:
 		print('You got it!')
 	else:
