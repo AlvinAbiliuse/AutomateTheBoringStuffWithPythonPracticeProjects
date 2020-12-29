705c4033043f7e24bf721877a648562788deb2a2
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-29 10:02:08 +0400

    collatz.py typo

--- Chapter 3/collatz.py
+++ Chapter 3/collatz.py
@@ -2,9 +2,9 @@
 
 def collatz(n):
 	if n == 1:
-		print(str"n")
+		print(str(n))
 	elif n % 2 == 0:
-		print(n // 2)
+		print(str(n // 2))
 		collatz(n // 2)
 	elif n % 2 == 1:
 		print(str(3 * n + 1))
