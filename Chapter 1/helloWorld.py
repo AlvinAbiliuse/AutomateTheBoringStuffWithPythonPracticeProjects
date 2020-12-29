c29d0465a0aca03dceff729db149206c7bd6cbe4
Alvin Abiliuse <alvinabiliuse@gmail.com>
2020-12-29 09:24:40 +0400

    helloWorld.py Chapter 1

---
+++ Chapter 1/helloWorld.py
@@ -0,0 +1,8 @@
+# This program says Hello and asks for my name.
+
+print('Hello World')
+print('What is your name?')	# ask for their name
+myName = input()
+print('It is good to meet you, ' + myName)
+print('The length of you name is:')
+print(len(myName))
