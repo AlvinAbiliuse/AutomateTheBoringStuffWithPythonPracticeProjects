303ebca30581951d56c1b73f98e7b8794288da9f
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 03:31:00 +0400

    putpixel14.png

--- Chapter 17/changingIndividualPixels.py
+++ Chapter 17/changingIndividualPixels.py
@@ -1,8 +1,12 @@
-from PIL import Image
+from PIL import Image, ImageColor
 
 im = Image.new('RGBA', (100, 100))
 for x in range(100):
 	for y in range(50):
 		im.putpixel((x, y), (210, 210, 210))
 
-im.save('putpixel14.png')
+for i in range(100):
+	for y in range(50, 100):
+		im.putpixel((x, y), ImageColor.getcolor('blue', 'RGB'))
+
+im.save('putpixel16.png')
