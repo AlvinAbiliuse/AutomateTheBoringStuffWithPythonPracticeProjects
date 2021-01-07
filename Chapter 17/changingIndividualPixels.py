297cd674f05aeb89df812e8bd0e2a6919b7812b7
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 03:33:04 +0400

    typo

--- Chapter 17/changingIndividualPixels.py
+++ Chapter 17/changingIndividualPixels.py
@@ -5,8 +5,8 @@ for x in range(100):
 	for y in range(50):
 		im.putpixel((x, y), (210, 210, 210))
 
-for i in range(100):
+for x in range(100):
 	for y in range(50, 100):
 		im.putpixel((x, y), ImageColor.getcolor('blue', 'RGB'))
 
-im.save('putpixel16.png')
+im.save('putpixel17.png')
