4e97a407c43ebe8de05e4e08d6c35d02ae4f8c58
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 03:35:10 +0400

    removed color values and used ImageColor.getcolor instead

--- Chapter 17/changingIndividualPixels.py
+++ Chapter 17/changingIndividualPixels.py
@@ -3,10 +3,10 @@ from PIL import Image, ImageColor
 im = Image.new('RGBA', (100, 100))
 for x in range(100):
 	for y in range(50):
-		im.putpixel((x, y), (210, 210, 210))
+		im.putpixel((x, y), ImageColor.getcolor('Red', 'RGB'))
 
 for x in range(100):
 	for y in range(50, 100):
 		im.putpixel((x, y), ImageColor.getcolor('blue', 'RGB'))
 
-im.save('putpixel17.png')
+im.save('putpixel18.png')
