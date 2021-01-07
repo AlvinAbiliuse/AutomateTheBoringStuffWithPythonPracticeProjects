b316f552a3917f59728b19903306dd5eb6ffdb34
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 03:58:06 +0400

    resizeAndAddLogo.py adjust height and width

--- Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
+++ Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
@@ -5,11 +5,18 @@ def edit(image):
 	editIm = Image.open(image)
 	w, h = editIm.size
 	print(f'{image} width: {w}, height: {h}')
-	if w > 300:
-		ow = w
-		w = 300
-		h = h - (ow - w)
-		print(f'    {image} new width: {w}, new height: {h}')
+	if w > h:
+		if w > 300:
+			ow = w
+			w = 300
+			h = h - (ow - w)
+			print(f'    {image} new width: {w}, new height: {h}')
+	elif h > w:
+		if h > 300:
+			oh = h
+			h = 300
+			w = w - (oh - h)
+			print(f'    {image} new width: {w}, new height: {h}')
 
 logoIm = Image.open('catlogo.png')
 
