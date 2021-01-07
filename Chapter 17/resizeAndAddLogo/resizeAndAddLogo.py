4c074c884eecfa1587db5778045993c59adc8ea4
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 03:55:15 +0400

    resizeAndAddLogo.py height compensation

--- Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
+++ Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
@@ -5,6 +5,11 @@ def edit(image):
 	editIm = Image.open(image)
 	w, h = editIm.size
 	print(f'{image} width: {w}, height: {h}')
+	if w > 300:
+		ow = w
+		w = 300
+		h = h - (ow - w)
+		print(f'    {image} new width: {w}, new height: {h}')
 
 logoIm = Image.open('catlogo.png')
 
