8553da47fdaec413c313a53c81e714fd8515beb9
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 04:30:30 +0400

    resizeAndAddLogo.py clean up

--- Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
+++ Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
@@ -4,24 +4,18 @@ import os
 def edit(image):
 	editIm = Image.open(os.path.join('Images', image))
 	w, h = editIm.size
-	print(f'{image} width: {w}, height: {h}')
-#	 resize not working properly
 	if w > h:
 		if w > 300:
 			ow = w
 			w = 300
 			ratio = h / ow 
 			h = int(w * ratio)
-			print(ratio)
-			print(f'    {image} new width: {w}, new height: {h}')
 	elif h > w:
 		if h > 300:
 			oh = h
 			h = 300
 			ratio = w / oh 
 			w = int(h * ratio)
-			print(ratio)
-			print(f'    {image} new width: {w}, new height: {h}')
 	resizedImage = editIm.resize((w, h))
 	resizedImage.save(f'./Resized/{image}')
 
