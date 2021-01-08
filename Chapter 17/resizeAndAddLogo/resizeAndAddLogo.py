5e1e69137e29783d5b1bab02e7bab66d6f342b63
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-09 03:46:06 +0400

    Progress with resizeAndAddLogo.py

--- Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
+++ Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import os
 
-def edit(image):
+def resize(image):
 	editIm = Image.open(os.path.join('Images', image))
 	w, h = editIm.size
 	if w > h:
@@ -17,9 +17,16 @@ def edit(image):
 			ratio = w / oh 
 			w = int(h * ratio)
 	resizedImage = editIm.resize((w, h))
-	resizedImage.save(f'./Resized/{image}')
+#	resizedImage.save(f'./Resized/{image}')
+	return addLogo(resizedImage, w, h, image)
+	
+
+def addLogo(image, width, height, name):
+	image.paste(logoIm, ((width - wi), (height - he)), logoIm)
+	image.save(f'./Resized/{name}')
 
 logoIm = Image.open('catlogo.png')
+wi, he = logoIm.size
 
 for i in os.listdir('./Images'):
-	edit(i)
+	mainImage = resize(i)
