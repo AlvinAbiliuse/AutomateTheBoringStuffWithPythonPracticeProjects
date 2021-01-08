985066e06730859f98eedf26a3d2a852e0409833
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 04:05:05 +0400

    commented out the resizing process

--- Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
+++ Chapter 17/resizeAndAddLogo/resizeAndAddLogo.py
@@ -5,20 +5,21 @@ def edit(image):
 	editIm = Image.open(os.path.join('Images', image))
 	w, h = editIm.size
 	print(f'{image} width: {w}, height: {h}')
-	if w > h:
-		if w > 300:
-			ow = w
-			w = 300
-			h = h - (ow - w)
-			print(f'    {image} new width: {w}, new height: {h}')
-	elif h > w:
-		if h > 300:
-			oh = h
-			h = 300
-			w = w - (oh - h)
-			print(f'    {image} new width: {w}, new height: {h}')
-	resizedImage = editIm.resize((w, h))
-	resizedImage.save(f'./resized/{image}')
+#	 resize not working properly
+#	if w > h:
+#		if w > 300:
+#			ow = w
+#			w = 300
+#			h = h - (ow - w)
+#			print(f'    {image} new width: {w}, new height: {h}')
+#	elif h > w:
+#		if h > 300:
+#			oh = h
+#			h = 300
+#			w = w - (oh - h)
+#			print(f'    {image} new width: {w}, new height: {h}')
+#	resizedImage = editIm.resize((w, h))
+#	resizedImage.save(f'./resized/{image}')
 
 logoIm = Image.open('catlogo.png')
 
