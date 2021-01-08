862d6fdd32e70060e8d02a460204b58c2be1f059
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-08 05:09:00 +0400

    ticTacToe.py

--- Chapter 5/ticTacToe.py
+++ Chapter 5/ticTacToe.py
@@ -1,6 +1,3 @@
-
-
-
 def printBoard(board):
 	print('\n  ' + board['top-L'] + ' | ' + board['top-M'] + ' | ' + board['top-R'])
 	print(' ---|---|---')
@@ -8,11 +5,23 @@ def printBoard(board):
 	print(' ---|---|---')
 	print('  ' + board['low-L'] + ' | ' + board['low-M'] + ' | ' + board['low-R'] + '\n')
 
-
-
-
-theBoard = {'top-L': 'X', 'top-M': 'O', 'top-R': 'X',
-			'mid-L': 'O', 'mid-M': 'X', 'mid-R': 'O',
-			'low-L': 'X', 'low-M': 'O', 'low-R': 'X'}
+theBoard = {'top-L': ' ', 'top-M': ' ', 'top-R': ' ',
+			'mid-L': ' ', 'mid-M': ' ', 'mid-R': ' ',
+			'low-L': ' ', 'low-M': ' ', 'low-R': ' '}
 
 printBoard(theBoard)
+for i in range(9):
+	for i in ['X', 'O']:
+		print(f'Enter Location for {i}:')
+		while True:
+			enteredKey = input()
+			try:
+				if theBoard[enteredKey] == ' ':
+					theBoard[enteredKey] = i
+				else:
+					print('Pick an empty spot!')
+					continue
+				break
+			except KeyError:
+				print('Try Again')
+		printBoard(theBoard)
