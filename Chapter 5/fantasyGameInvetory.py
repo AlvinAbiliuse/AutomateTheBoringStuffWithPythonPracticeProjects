5fa30467c26edc94a1df8f4bc68c75334c0cbab7
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-09 06:32:20 +0400

    progress on fantasyGameInvetory.py

--- Chapter 5/fantasyGameInvetory.py
+++ Chapter 5/fantasyGameInvetory.py
@@ -1,13 +1,10 @@
-
-stuff = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-
 def displayInventory(inventory):
 	print('Inventory:')
 	item_total = 0
 	for k, v in inventory.items():
 		print(str(v) + ' ' + k)
 		item_total += v
-		print('Total number of items: ' + str(item_total))
+	print('\nTotal number of items: ' + str(item_total))
 
 def addToInventory(inventory, addItems):
 	tech = inventory
@@ -19,8 +16,9 @@ def addToInventory(inventory, addItems):
 	print(tech)
 	return tech
 	
-displayInventory(stuff)
+inv = {'gold coin': 42, 'rope': 1}
+displayInventory(inv)
 print('')
 dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-inv = addToInventory(stuff, dragonLoot)
+inv = addToInventory(inv, dragonLoot)
 displayInventory(inv)
