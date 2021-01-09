a7c65ec0c68df6a29d8f503a80ee409e1272dc45
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-09 06:17:39 +0400

    progress on fantasyGameInvetory.py

--- Chapter 5/fantasyGameInvetory.py
+++ Chapter 5/fantasyGameInvetory.py
@@ -9,4 +9,13 @@ def displayInventory(inventory):
 		item_total += v
 		print('Total number of items: ' + str(item_total))
 
+def addToInventory(inventory, addItems):
+	for i in addItems:
+		if i in inventory.items():
+			inventory[i] = inventory[i] + 1
+	
 displayInventory(stuff)
+print('')
+dragonLoot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
+inv = addToInventory(stuff, dragonLoot)
+displayInventory(inv)
