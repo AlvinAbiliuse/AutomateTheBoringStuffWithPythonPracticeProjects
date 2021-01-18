faabfb1277ff9b3e8e4da3d53b1e80817a20fd4a
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-18 04:32:54 +0400

    progress with seatingCards.py

--- Chapter 17/seatingCards/seatingCards.py
+++ Chapter 17/seatingCards/seatingCards.py
@@ -1,3 +1,9 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 
-
+im = Image.new('RGBA', (360 * 2, 288 * 2), 'white')
+draw = ImageDraw.Draw(im)
+draw.text((50, 50), 'Welcome Master Anikin', fill='black')
+flowerIm = Image.open('img.png').convert('RGBA')
+resizedFlowerIm = flowerIm.resize((260 * 2, 188 * 2))
+im.paste(resizedFlowerIm, (50 * 2, 50 * 2), resizedFlowerIm)
+im.save('flowerImTextLarger.png')
