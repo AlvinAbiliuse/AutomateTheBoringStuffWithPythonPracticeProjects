f02057e9489116fe384a6bb6d8c834a83db82ec4
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-01-27 12:47:51 +0400

    picnicTable.py done

--- Chapter 6/picnicTable.py
+++ Chapter 6/picnicTable.py
@@ -1,6 +1,6 @@
 def printPicnic(itemDict, leftWidth, rightWidth):
 	print('PICNIC ITEMS'.center(leftWidth + rightWidth, '-'))
-	for k, v in itemsDict.items():
+	for k, v in itemDict.items():
 		print(k.ljust(leftWidth, '.') + str(v).rjust(rightWidth))
 
 picnicItems = {'sandwiches': 4, 'apples': 12, 'cups': 4, 'cookies': 8000}
