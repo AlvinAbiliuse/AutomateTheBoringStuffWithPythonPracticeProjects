be1f646f0ddb55ab9ddf2236f571055c5951cded
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-02-02 06:26:21 +0400

    tablePrinter.py done

--- Chapter 6/tablePrinter/tablePrinter.py
+++ Chapter 6/tablePrinter/tablePrinter.py
@@ -4,24 +4,25 @@ tableData = [['apples', 'oranges', 'cherries', 'banana'],
 
 
 
-def scanData(num, data):
+def scanData(num):
+	data = 0
 	for i in tableData[num]:
 		if len(i) > data:
 			data = len(i)
+	return data
 
-def pprinttable(data):
+def pprinttable(data, w0, w1, w2):
+	order = [w0, w1, w2]
 	for i in range(len(data[0])):
 		for j in range(len(data)):
+			print(' ' * ((order[j] - len(data[j][i]) + 1)), end='')
 			print(f'{data[j][i]} ', end='')
 		print('')
 
-width0 = 0
-scanData(0, width0)
+width0 = scanData(0)
 
-width1 = 0
-scanData(1, width1)
+width1 = scanData(1)
 
-width2 = 0
-scanData(2, width2)
+width2 = scanData(2)
 
-pprinttable(tableData)
+pprinttable(tableData, width0, width1, width2)
