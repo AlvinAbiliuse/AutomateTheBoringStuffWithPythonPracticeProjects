b09c3c54b45b9ffd528b9a77db1563aa14641fc4
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-02-24 02:08:52 +0400

    test.py

---
+++ Chapter 18/test.py
@@ -0,0 +1,12 @@
+import sys
+import time
+
+n = 1
+
+while True:
+	tt = 'Hello World! ' + str(n)
+	sys.stdout.write(tt)
+	sys.stdout.flush()
+	time.sleep(1)
+	sys.stdout.write('\b' * len(tt))
+	n = n + 1
