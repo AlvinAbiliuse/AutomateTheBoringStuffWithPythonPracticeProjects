c14d82d1f2ed84ec0ff3b220a92b2e6fb01df5da
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-02-24 02:21:52 +0400

    mouseNow.py almost done

--- Chapter 18/mouseNow.py
+++ Chapter 18/mouseNow.py
@@ -6,7 +6,8 @@ pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
 while True:
-	position = str(pyautogui.position())
+	x, y = pyautogui.position()
+	position = f'x={x}, y={y}'
 	sys.stdout.write(position)
 	sys.stdout.flush()
 	sys.stdout.write('\b' * len(position))
