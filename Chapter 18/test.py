0b37726d1e3518770eff196548af02bdc48ea4bc
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-02-24 02:14:51 +0400

    test.py done

--- Chapter 18/test.py
+++ Chapter 18/test.py
@@ -3,12 +3,14 @@ import time
 
 n = 1
 
+# stdout stores data which is sent to the terminal when flush() is
+# called. using '\b' clears out the stored stdout data for it to be 
+# filled again
+
 while True:
 	tt = 'Hello World! ' + str(n)
 	sys.stdout.write(tt)
 	sys.stdout.flush()
-	time.sleep(1)
-	sys.stdout.write('hooo')
 	time.sleep(1)
 	sys.stdout.write('\b' * len(tt))
 	n = n + 1
