65c4fe9311dbb07acb42ae64263596f305719732
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-02-24 02:18:18 +0400

    mouseNow.py progress

--- Chapter 18/mouseNow.py
+++ Chapter 18/mouseNow.py
@@ -1,7 +1,12 @@
 import pyautogui
+import sys
+import time
 
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
 while True:
-	print(pyautogui.position())
+	position = str(pyautogui.position())
+	sys.stdout.write(position)
+	sys.stdout.flush()
+	sys.stdout.write('\b' * len(position))
