31680997d87d46772ab676dc361663a269bc0489
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-02-24 02:52:55 +0400

    mouseNow2.py completed

--- Chapter 18/mouseNow2.py
+++ Chapter 18/mouseNow2.py
@@ -1,8 +1,11 @@
-
 import pyautogui
 
-while True:
-	x, y = pyautogui.position()
-	tt = f'x: {x}, y: {y}'
-	print(tt, end='')
-	print('\b' * len(tt), end='')
+try:
+	print('Press Ctrl + C to close')
+	while True:
+		x, y = pyautogui.position()
+		tt = f'x: {x}, y: {y}'
+		print(tt, end='')
+		print('\b' * len(tt), end='')
+except KeyboardInterrupt:
+	print('', end='')
