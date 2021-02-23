69cdb0fd994d49da739e72a54c8888c1eaca26d3
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-02-24 02:58:29 +0400

    mouseNow.py exception for KeyboardInterrupt

--- Chapter 18/mouseNow.py
+++ Chapter 18/mouseNow.py
@@ -11,9 +11,13 @@ import sys
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
-while True:
-	x, y = pyautogui.position()
-	position = f'x={x}, y={y}'
-	sys.stdout.write(position)
-	sys.stdout.flush()
-	sys.stdout.write('\b' * len(position))
+try:
+	print('Press Ctrl + C to exit')
+	while True:
+		x, y = pyautogui.position()
+		position = f'x={x}, y={y}  '
+		sys.stdout.write(position)
+		sys.stdout.flush()
+		sys.stdout.write('\b' * len(position))
+except KeyboardInterrupt:
+	print('', end='')
