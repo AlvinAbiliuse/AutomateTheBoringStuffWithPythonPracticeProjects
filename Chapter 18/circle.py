4fbbcddcf325b59d3c7783b03d386ae4e058d6cf
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-03-08 03:13:12 +0400

    circle.py cannot get a proper arc

--- Chapter 18/circle.py
+++ Chapter 18/circle.py
@@ -6,17 +6,23 @@ pyautogui.click()
 
 def arch():
 	radius = 45
-	vertices = 45
+	vertices = 10
 	length = 300
 	
 	newRad = radius / vertices
 	newLen = length / vertices
 	
+	print(f'newRad: {newRad}')
+	print(f'newLen: {newLen}', end='\n\n')
+	
 	x = newLen
 	y = 0
 	for i in range(vertices):
 		pyautogui.dragRel(x, y, duration=0.2)
-		x = x - newLen
-		y = y + newLen
+		print(f'x: {x}')
+		print(f'y: {y}', end='\n\n')
+		x = x - newRad
+		y = y + newRad
+		
 
 arch()
