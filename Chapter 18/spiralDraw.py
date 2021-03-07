ddc2aa85077877470e993c04e741aedfa9809314
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-03-08 02:51:13 +0400

    spiralDraw.py complete

--- Chapter 18/spiralDraw.py
+++ Chapter 18/spiralDraw.py
@@ -1,6 +1,15 @@
 import pyautogui, time
 
+time.sleep(5)
+
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
-
-
+pyautogui.click()
+distance = 200
+while distance > 0:
+	pyautogui.dragRel(distance, 0, duration=0.2) # Move Right
+	distance = distance - 5
+	pyautogui.dragRel(0, distance, duration=0.2) # Move Down
+	pyautogui.dragRel(-distance, 0, duration=0.2) # Move Left
+	distance = distance - 5
+	pyautogui.dragRel(0, -distance, duration=0.2) # Move Up
