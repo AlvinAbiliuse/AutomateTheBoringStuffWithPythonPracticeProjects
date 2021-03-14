ad48546054dd52b9f5a911f377933d49152286b9
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-03-14 08:08:20 +0400

    mouseNow2.py progress

--- Chapter 18/mouseNow2.py
+++ Chapter 18/mouseNow2.py
@@ -4,14 +4,15 @@
 # version of mouseNow that uses print statements instead of sys
 
 import pyautogui
+import time
 
 try:
 	print('Press Ctrl + C to close')
 	while True:
 		x, y = pyautogui.position()
 		r, g, b = pyautogui.screenshot().getpixel((x, y))
-		tt = f'x: {x}, y: {y}  RGB: {str(pixelColor[0])}, {str(pixelColor[1])}, {str(pixelColor[2])}'
-		print(tt, end='')
+		tt = f'x: {x}, y: {y}  RGB: {str(r)}, {str(g)}, {str(b)}'
+		print(tt, end='', flush=True)
 		print('\b' * len(tt), end='')
 except KeyboardInterrupt:
 	print('', end='')
