97016323d79d53be908290f6dd86a1e2e5475c91
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-20 06:33:42 +0400

    formFiller.py progress

--- Chapter 18/formFiller/formFiller.py
+++ Chapter 18/formFiller/formFiller.py
@@ -1,4 +1,6 @@
 import pyautogui
+import time
+
 
 def formFiller():
 	pyautogui.click(20, 80)
@@ -7,7 +9,15 @@ def formFiller():
 	msg = list('https://docs.google.com/forms/d/e/1FAIpQLScSVDFU76rZvbO_tiIwSt6d9sOK0CZyS9KKMCP6cP5O5W5lVQ/viewform')
 	msg.append('enter')
 	pyautogui.typewrite(msg)
-
+	time.sleep(5)
+	pyautogui.click(281, 173)
+	for i in range(2):
+		pyautogui.typewrite('tab')
+		time.sleep(1)
+	pyautogui.typewrite('Alvin')
+	pyautogui.typewrite('tab')
+	time.sleep(1)
+	pyautogui.typewrite('Heights')
 
 
 if __name__ == "__main__":
