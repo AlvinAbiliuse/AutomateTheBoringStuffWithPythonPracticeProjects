8ff434e58144ec82725aa6c3a1372263d4dfaa49
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-20 06:45:49 +0400

    formFiller.py moar progress

--- Chapter 18/formFiller/formFiller.py
+++ Chapter 18/formFiller/formFiller.py
@@ -1,7 +1,7 @@
 import pyautogui
 import time
 
-
+pyautogui.PAUSE = 1
 def formFiller():
 	pyautogui.click(20, 80)
 	pyautogui.hotkey('ctrl', 't')
@@ -13,12 +13,22 @@ def formFiller():
 	pyautogui.click(281, 173)
 	for i in range(2):
 		pyautogui.typewrite('\t')
-		time.sleep(1)
 	pyautogui.typewrite('Alvin')
 	pyautogui.typewrite('\t')
-	time.sleep(1)
 	pyautogui.typewrite('Heights')
-
+	pyautogui.typewrite('\t')
+	pyautogui.typewrite('enter')
+	for i in range(2):
+		pyautogui.typewrite('down')
+	pyautogui.typewrite('enter')
+	pyautogui.typewrite('\t')
+	for i in range(4):
+		pyautogui.typewrite('right')
+	for i in range(2):
+		pyautogui.typewrite('\t')
+	pyautogui.typewrite('No additional comments!')
+	
+	
 
 if __name__ == "__main__":
 	formFiller()
