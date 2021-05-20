6ecde8af216612351d1217797437a3fbc6a85c35
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-05-20 06:18:44 +0400

    formFiller.py added link

--- Chapter 18/formFiller/formFiller.py
+++ Chapter 18/formFiller/formFiller.py
@@ -1,6 +1,12 @@
 import pyautogui
 
 def formFiller():
+	pyautogui.click(20, 80)
+	pyautogui.hotkey('ctrl', 't')
+
+	msg = list('https://docs.google.com/forms/d/e/1FAIpQLScSVDFU76rZvbO_tiIwSt6d9sOK0CZyS9KKMCP6cP5O5W5lVQ/viewform')
+	msg.append('enter')
+	pyautogui.typewrite(msg)
 
 
 
