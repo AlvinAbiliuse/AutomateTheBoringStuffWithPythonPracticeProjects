6df032713cec7d786eb668180c4d7f948a5b48d4
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-06-11 04:51:46 +0400

    formFiller2.py

---
+++ Chapter 18/formFiller/formFiller2.py
@@ -0,0 +1,22 @@
+
+
+import pyautogui
+
+
+name = Point(x=270, y=252)
+weakness = Point(x=277, y=339)
+wizard = Point(x=285, y=424)
+choice1 = Point(x=285, y=452)
+choice2 = Point(x=290, y=479)
+choice3 = Point(x=277, y=500)
+choice4 = Point(x=285, y=526)
+escape = Point(x=448, y=431)
+robo1 = Point(x=316, y=534)
+robo2 = Point(x=348, y=535)
+robo3 = Point(x=377, y=530)
+robo4 = Point(x=406, y=535)
+robo5 = Point(x=432, y=535)
+addCom = Point(x=327, y=630)
+submit = Point(x=233, y=663)
+
+
