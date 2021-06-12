2b8d917a3d0ec19021377cbb05d6e9c985788e74
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-06-12 05:30:22 +0400

    formFiller2.py moved code inside function

--- Chapter 18/formFiller/formFiller2.py
+++ Chapter 18/formFiller/formFiller2.py
@@ -7,33 +7,37 @@ import time
 pyautogui.PAUSE = 1
 pyautogui.FAILSAFE = True
 
-pyautogui.click(22, 82)
-pyautogui.hotkey('ctrl', 't')
-pyautogui.click(379, 86)
-msg = list('https://docs.google.com/forms/d/e/1FAIpQLScSVDFU76rZvbO_tiIwSt6d9sOK0CZyS9KKMCP6cP5O5W5lVQ/viewform')
-msg.append('enter')
-pyautogui.typewrite((msg))
-time.sleep(3)
-# name 
-pyautogui.click(270, 252)
-pyautogui.typewrite('Dino')
-# weakness 
-pyautogui.click(277, 330)
-pyautogui.typewrite('Handshakes')
-# wizard 
-pyautogui.click(285, 420)
-choice = [(285, 452), (290, 479), (277, 500), (285, 526)]
-random.shuffle(choice)
-print(choice[0])
-pyautogui.click(choice[0])
-# escape 
-pyautogui.click(448, 431)
-robo  = [(318, 510), (346, 511), (375, 510), (404, 510), (434, 510)]
-random.shuffle(robo)
-pyautogui.click(robo[0])
-# addCom 
-pyautogui.click(256, 604)
-pyautogui.typewrite('No Additional Comments.')
-# submit
-time.sleep(5)
-pyautogui.click(240, 642)
+def formFill():
+	pyautogui.click(22, 82)
+	pyautogui.hotkey('ctrl', 't')
+	pyautogui.click(379, 86)
+	msg = list('https://docs.google.com/forms/d/e/1FAIpQLScSVDFU76rZvbO_tiIwSt6d9sOK0CZyS9KKMCP6cP5O5W5lVQ/viewform')
+	msg.append('enter')
+	pyautogui.typewrite((msg))
+	time.sleep(3)
+	# name 
+	pyautogui.click(270, 252)
+	pyautogui.typewrite('Dino')
+	# weakness 
+	pyautogui.click(277, 330)
+	pyautogui.typewrite('Handshakes')
+	# wizard 
+	pyautogui.click(285, 420)
+	choice = [(285, 452), (290, 479), (277, 500), (285, 526)]
+	random.shuffle(choice)
+	print(choice[0])
+	pyautogui.click(choice[0])
+	# escape 
+	pyautogui.click(448, 431)
+	robo  = [(318, 510), (346, 511), (375, 510), (404, 510), (434, 510)]
+	random.shuffle(robo)
+	pyautogui.click(robo[0])
+	# addCom 
+	pyautogui.click(256, 604)
+	pyautogui.typewrite('No Additional Comments.')
+	# submit
+	time.sleep(5)
+	pyautogui.click(240, 642)
+
+if __name__ == "__main__":
+	formFill()
