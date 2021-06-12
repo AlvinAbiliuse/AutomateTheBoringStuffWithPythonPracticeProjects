660c9389d41aaa705240989bc8b616d23a56847f
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-06-12 05:34:49 +0400

    changed names

---
+++ Chapter 18/formFiller/nonWorkingFormFiller.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+
+import pyautogui
+import time
+
+pyautogui.PAUSE = 1
+def formFiller():
+	pyautogui.click(20, 80)
+	pyautogui.hotkey('ctrl', 't')
+
+	msg = list('https://docs.google.com/forms/d/e/1FAIpQLScSVDFU76rZvbO_tiIwSt6d9sOK0CZyS9KKMCP6cP5O5W5lVQ/viewform')
+	msg.append('enter')
+	pyautogui.typewrite(msg)
+	time.sleep(5)
+	pyautogui.click(281, 173)
+	for i in range(2):
+		pyautogui.typewrite('\t')
+	pyautogui.typewrite('Alvin')
+	pyautogui.typewrite('\t')
+	pyautogui.typewrite('Heights')
+	pyautogui.typewrite('\t')
+	pyautogui.typewrite('enter')
+	for i in range(1):
+		pyautogui.typewrite('down')
+	pyautogui.typewrite('enter')
+	pyautogui.typewrite('\t')
+	for i in range(3):
+		pyautogui.typewrite('right')
+	pyautogui.typewrite('enter')
+	for i in range(2):
+		pyautogui.typewrite('\t')
+	pyautogui.typewrite('No additional comments!')
+	
+	
+
+if __name__ == "__main__":
+	formFiller()
