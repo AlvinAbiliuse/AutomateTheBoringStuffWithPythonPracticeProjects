cf24ea4f03046bb259bde4ec9485ad1b61eabc10
Alvin Abiliuse <alvinabiliuse@gmail.com>
2021-06-23 05:15:13 +0400

    lookinBusy.py completed

--- Chapter 18/lookingBusy/lookinBusy.py
+++ Chapter 18/lookingBusy/lookinBusy.py
@@ -1,2 +1,15 @@
 
 # lookingBusy.py
+
+import pyautogui
+
+pyautogui.FAILSAFE = True
+pyautogui.PAUSE = 5
+def busy():
+	while True:
+		pyautogui.moveRel((0, 5))
+		pyautogui.moveRel((0, -5))
+
+if __name__ == "__main__":
+	busy()
+		
